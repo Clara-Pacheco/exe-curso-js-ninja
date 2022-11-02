74b4184d94a7007a12815b2821e66b86126011ea
Clara <clarapachecodev@gmail.com>
2022-11-02 13:54:13 -0300

    create event for operation buttons and function to check if an operation already exists

--- SECAO 23 - AULA 23/desafio23.js
+++ SECAO 23 - AULA 23/desafio23.js
@@ -6,32 +6,33 @@
     As regras são:
 
     - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-    diretamente;
+    diretamente; OK
 
-    - O input deve iniciar com valor zero;
+    - O input deve iniciar com valor zero; OK
 
-    - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+    - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número; OK
 
     - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-    multiplicação(x) e divisão(÷);
+    multiplicação(x) e divisão(÷); OK
 
     - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-    que irá limpar o input, deixando-o com valor 0;
+    que irá limpar o input, deixando-o com valor 0; OK
 
     - A cada número pressionado, o input deve atualizar concatenando cada valor
-    digitado, como em uma calculadora real;
+    digitado, como em uma calculadora real; OK
 
     - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
     operação no input. Se o último caractere no input já for um símbolo de alguma
     operação, esse caractere deve ser substituído pelo último pressionado.
 
     Exemplo:
+
     - Se o input tem os valores: "1+2+", e for pressionado o botão de
     multiplicação (x), então no input deve aparecer "1+2x".
 
     - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
     input;
-    - Ao pressionar o botão "CE", o input deve ficar zerado.
+    - Ao pressionar o botão "CE", o input deve ficar zerado. OK
     */
 
     let input = document.querySelector(".userInput");
@@ -47,6 +48,7 @@
     const number0 = document.querySelector(".button0");
 
     const allbuttons = document.querySelectorAll(".allButtons")
+    const allbuttonsOperations = document.querySelectorAll(".buttonsOperations")
 
     const buttonAddition = document.querySelector(".buttonAddition");
     const buttonSubtraction = document.querySelector(".buttonSubtraction");
@@ -84,17 +86,61 @@
     // callback do método forEach que pode receber o elemento e seu index. No exemplo da calculadora,
     // cada elemento do forEach será cada botão de número
 
+    /*Separamos todos os eventos em um único lugar */
+
     Array.prototype.forEach.call(allbuttons, function(button){
       button.addEventListener('click', handleClickNumber)
     });
 
-    // dentro da função handClickNumber, temos acesso a cada elemento, ou seja, a cada botão - 
-    // o 'this' dentro da função é o próprio botão que está sendo precionado.
-    // Se colocarmos .value, teremos ao valor do botão precionado.
+    Array.prototype.forEach.call(allbuttonsOperations,function(button){
+      button.addEventListener('click', handleClickOperation)
+    })
 
+    /* 1 - dentro da função handClickNumber, temos acesso a cada elemento, ou seja, a cada botão - 
+    o 'this' dentro da função é o próprio botão que está sendo precionado.
+    Se colocarmos .value, teremos ao valor do botão precionado.*/
+
+    // Quando botão CE for pressionado
+
+    buttonCE.addEventListener('click', function(){
+      input.value = 0;
+    })
+
+    // 2- Dividimos cada responsabilidade para uma função diferente
+
+    
     function handleClickNumber(e){
       // console.log(this.value);
       input.value += this.value;
       // irá pegar o valor do input e concatenar com o valor do botão clicado
     }
+
+    function handleClickOperation(){
+      
+      if(islastItemOperation()){
+        input.value = input.value.slice(0,-1)
+      // se o último elemento for igual a um elemento pertencente ao array operations,
+      // esse item será removido para que o novo(logo abaixo) seja adicionado.
+      // Com o método slice, começamos no índice 0 e vamos até o -1, ou seja, o último
+      // do array, mas o último numca é pego no método slice, ele sempre fica de fora. 
+      // Na prática, é pego até o elemento de índice -2. Ou seja, o elemento de índice -1
+      // é eliminado.
+      }
+      input.value += this.value;  
+    }
+
+    function islastItemOperation(){
+      let operations = ['+','-', 'x', '÷'];
+      let lastItem = input.value.split('').pop();
+      return operations.some(function(operator){
+        return operator === lastItem;
+      });
+      console.log(lastItem)
+    }
+
+
+
+   
+
+
 })();
